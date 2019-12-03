8c3796cacf8f755bc35b26b7bc53558f32891afe
dem4ply <dem4ply@gmail.com>
2019-12-03 17:12:05 -0600

    "Linux: the operating system with a CLUE... Command Line User Environment". (seen in a posting in comp.software.testing)

--- excel_generator/cli.py
+++ excel_generator/cli.py
@@ -4,6 +4,9 @@ import logging
 import sys
 
 
+from chibi.file import Chibi_path
+
+
 logger_formarter = '%(levelname)s %(name)s %(asctime)s %(message)s'
 
 
@@ -14,6 +17,11 @@ def main():
     parser.add_argument(
         '-w', '--white_rows', dest='white_rows', default=5, type=int )
     parser.add_argument(
+        '-r', '--rows', dest='rows', default=5, type=int )
+    parser.add_argument(
+        "-o", "--output", type=Chibi_path, dest="output",
+        help="direccion del archivo de xlxs" )
+    parser.add_argument(
         "--log_level", dest="log_level", default="INFO",
         help="log level 🍡",
     )
@@ -21,6 +29,11 @@ def main():
     logging.basicConfig( level=args.log_level, format=logger_formarter )
 
     print( args.white_rows )
+
+    if args.output.exists:
+        print( "el archivo existe" )
+    else:
+        print( "el archivo no existe" )
     return 0
 
 
