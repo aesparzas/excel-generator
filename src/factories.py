29174ef0cde3ea2ab177f324798245c47b977031
DRMD431LU <drmdfoureilu@gmail.com>
2019-12-04 12:45:59 -0600

    add base fields to factories

--- src/factories.py
+++ src/factories.py
@@ -1,12 +1,13 @@
 import factory
 import random
 from faker import Factory as faker_factory
-
+from datetime import timedelta
 
 fake = faker_factory.create()
 MODE = ['TRUCK', 'LTL', 'VESSEL OCEAN', 'RAIL', 'PLANE']
 FREIGHT = ['I', 'O', 'B']
-
+ten_days = timedelta( days=10 )
+one_day = timedelta( days=1 )
 
 class LTL():
 	pass
@@ -31,54 +32,67 @@ class Record(factory.Factory):
 	geography_exclusion = factory.lazy_attribute( lambda x: fake.pybool() )
 	origin_location_code = 'LAX|LGB|VAN'
 	origin_region = ''
-	origin_city = factory.lazy_attribute( lambda x: str(
-						fake.city()) + str(
+	origin_city = factory.lazy_attribute( lambda x: str( fake.city() ) + str(
 						fake.country_code(representation="alpha-2"))  )
 	origin_county = ''
-	origin_state = ''
-	origin_zip = ''
+	origin_state = factory.LazyAttribute( lambda x: fake.state_abbr(include_territories=True) )
+	origin_zip = factory.LazyAttribute( lambda x:  fake.zipcode() )
 	origin_zip_range = ''
 	origin_country = factory.LazyAttribute( lambda x: fake.country() )
-	destination_location_code = ''
+	destination_location_code = 'PVG|SHA'
 	destination_region = ''
-	destination_city = ''
-	destination_county = factory.LazyAttribute( lambda x: fake.country() )
-	destination_state = ''
-	destination_zip = ''
+	destination_city = factory.lazy_attribute( lambda x: str( fake.city() ) + str(
+						fake.country_code(representation="alpha-2"))  )
+	destination_county = ''
+	destination_state = factory.LazyAttribute( lambda x: fake.state_abbr(include_territories=True) )
+	destination_zip = factory.LazyAttribute( lambda x:  fake.zipcode() )
 	destination_zip_range = ''
-	destination_country = ''
+	destination_country = factory.LazyAttribute( lambda x: fake.country() )
 	efective_date = factory.lazy_attribute(
 							lambda x: fake.date_time_between(
 							start_date="now", end_date="+15d", tzinfo=None ) )
-	expiration_date = ''
+	expiration_date = factory.lazy_attribute(
+			lambda self: fake.date_time_between_dates(
+			datetime_end=self.authority_effective_date + ten_days,
+        	datetime_start=self.authority_effective_date + one_day,
+        	tzinfo=None ) )
 	load_id = ''
-	chargetype = ''
-	uom = ''
+	chargetype = 'LT'
+	uom = 'ML'
 	instance = ''
 	auto_apply = ''
 	apply_at_level = ''
-	band_uom_min = ''
-	band_uom_max = ''
-	flat_charge = ''
-	rate = ''
+	band_uom_min = '0'
+	band_uom_max = '10'
+	flat_charge = '10.0'
+	rate = '2.65'
 	minimum_charge = ''
 	maximum_charge = ''
 	base_charge = ''
 	excess_rate = ''
-	discount_rate = ''
-	currency_code = ''
+	discount_rate = '78.70'
+	currency_code = 'USD'
 	allow_deficit = factory.lazy_attribute( lambda x: fake.pybool() )
 	allow_higher_down = factory.lazy_attribute( lambda x: fake.pybool() )
-	authority_publisher = ''
-	authority_release = ''
-	authority_section = ''
-	authority_item = ''
-	authority_effective_date = ''
-	authority_expiration_date = ''
-	low_commodity_code = ''
+	authority_publisher = 'CZ'
+	authority_release = 'AA'
+	authority_section = 'REV1'
+	authority_item = 'IL4 IB'
+	authority_effective_date = factory.lazy_attribute(
+							lambda self: fake.date_time_between(
+							start_date=self.efective_date + one_day, end_date="+14d", tzinfo=None ) )
+	authority_expiration_date = factory.lazy_attribute(
+							lambda self: fake.date_time_between(
+							start_date=self.expiration_date + one_day, end_date="+14d", tzinfo=None ) )
+	low_commodity_code = factory.lazy_attribute( lambda x: fake.pyfloat(
+							positive=True, min_value=120, max_value=500) )
 	high_commodity_code = ''
-	low_class = ''
-	high_class = ''
+	low_class = factory.lazy_attribute(
+					lambda self: self.low_class_aux + fake.pyfloat(
+					positive=True, min_value=50, max_value=150, right_digits=2 ) )
+	high_class = factory.lazy_attribute(
+					lambda self: self.low_class + fake.pyfloat(
+					positive=True, min_value=50, max_value=250, right_digits=4 ) )
 	as_class = ''
 	constraint = ''
 
